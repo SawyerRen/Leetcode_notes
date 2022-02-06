fbfb76ba41a47bb055044b7955f34025a02b7caf
SawyerRen <923184985@qq.com>
2022-02-06 10:15:11 -0600

    165

--- codes/src/all_solution/q100/Q161.java
+++ codes/src/all_solution/q100/Q161.java
@@ -3,23 +3,23 @@ package all_solution.q100;
 public class Q161 {
     public boolean isOneEditDistance(String s, String t) {
         if (Math.abs(s.length() - t.length()) > 1) return false;
-        int count = 0;
-        int i = 0;
-        // 保证s长度大于等于t
         if (s.length() < t.length()) return isOneEditDistance(t, s);
         if (s.length() == t.length()) {
-            while (i < s.length()) {
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) != t.charAt(i)) count++;
-                if (count > 1) return false;
-                i++;
             }
-            return count == 1;
+            if (count != 1) return false;
         } else {
             if (t.length() == 0) return true;
+            int i = 0;
             while (i < t.length()) {
-                if (s.charAt(i) != t.charAt(i)) return s.substring(i + 1).equals(t.substring(i));
+                if (s.charAt(i) != t.charAt(i)) {
+                    return s.substring(i + 1).equals(t.substring(i));
+                }
                 i++;
             }
+            return true;
         }
         return true;
     }
--- codes/src/all_solution/q100/Q163.java
+++ codes/src/all_solution/q100/Q163.java
@@ -7,21 +7,25 @@ public class Q163 {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> res = new ArrayList<>();
         if (nums.length == 0) {
-            add2List(res, lower, upper);
+            res.add(toStr(lower, upper));
             return res;
         }
-        if (lower < nums[0]) add2List(res, lower, nums[0] - 1);
+        if (lower < nums[0]) {
+            res.add(toStr(lower, nums[0] - 1));
+        }
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i + 1] - nums[i] > 1) {
-                add2List(res, nums[i] + 1, nums[i + 1] - 1);
+                res.add(toStr(nums[i] + 1, nums[i + 1] - 1));
             }
         }
-        if (nums[nums.length - 1] < upper) add2List(res, nums[nums.length - 1] + 1, upper);
+        if (nums[nums.length - 1] < upper) {
+            res.add(toStr(nums[nums.length - 1] + 1, upper));
+        }
         return res;
     }
 
-    private void add2List(List<String> res, int lower, int upper) {
-        if (lower == upper) res.add(String.valueOf(lower));
-        else res.add(lower + "->" + upper);
+    private String toStr(int lower, int upper) {
+        if (lower == upper) return String.valueOf(lower);
+        return lower + "->" + upper;
     }
 }
